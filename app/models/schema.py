8a0bb8abe373a14955d1e51a0af70ff306b40fa0
Dominik Hajko <xhajkod@stuba.sk>
2026-02-06 17:11:23 +0100

    Fixes to shcema and requiremets

--- app/models/schema.py
+++ app/models/schema.py
@@ -34,7 +34,7 @@ class Road(BaseModel):
 
 
 class Junction(BaseModel):
-    model_config = ConfigDict(extra="ignore")
+    model_config = ConfigDict(extra="ignore", arbitrary_types_allowed=True)
 
     junction_id: constr(min_length=1, max_length=64)
     connected_roads_count: conint(ge=0, le=2**32 - 1) = 0 # uint32_t
@@ -48,7 +48,7 @@ class Junction(BaseModel):
 
     @model_validator(mode="after")
     def _default_polygon(self):
-        if self.polygon is None:
+        if self.polygon is None and self.x is not None and self.y is not None:
             half = self.junction_size / 2
             self.polygon = Polygon([(self.x - half, self.y - half),
                                     (self.x + half, self.y - half),
